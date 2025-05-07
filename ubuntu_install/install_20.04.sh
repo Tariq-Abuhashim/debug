923e25bebf14761027940d16c02a914feb89a228
Tariq Abuhashim <t.abuhashim@gmail.com>
2025-05-08 01:50:26 +1200

    Update install_20.04.sh

--- ubuntu_install/install_20.04.sh
+++ ubuntu_install/install_20.04.sh
@@ -99,6 +99,7 @@ cd ~/Downloads
 # or
 wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.0.1/local_repo/nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-11.8_1.0-1_amd64.deb
 sudo dpkg -i nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-11.8_1.0-1_amd64.deb
+sudo cp /var/nv-tensorrt-local-repo-ubuntu2004-10.0.1-cuda-11.8/nv-tensorrt-local-4BE0C9B6-keyring.gpg /usr/share/keyrings/
 sudo apt update
 sudo apt install tensorrt
 sudo apt install python3-libnvinfer-dev
