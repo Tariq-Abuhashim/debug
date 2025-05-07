2764ec1799715b9e3f4bc251c666c5516de38465
Tariq Abuhashim <t.abuhashim@gmail.com>
2025-05-08 00:55:19 +1200

    Update install_20.04.sh

--- ubuntu_install/install_20.04.sh
+++ ubuntu_install/install_20.04.sh
@@ -34,8 +34,8 @@ wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/
 sudo dpkg -i cuda-repo-ubuntu2004-11-4-local_11.4.0-470.42.01-1_amd64.deb
 sudo apt-key add /var/cuda-repo-ubuntu2004-11-4-local/7fa2af80.pub
 sudo apt-get update
-#sudo apt-get -y install cuda
-sudo apt-get -y install cuda-11.4
+#sudo apt-get install cuda
+sudo apt-get install cuda-11.4
 
 echo 'export PATH=/usr/local/cuda-11.4/bin:$PATH' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
